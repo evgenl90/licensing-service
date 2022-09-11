135fe24e801d3690e20911a4127c19ad64444efb
Evgen <evgen.990029@gmail.com>
2022-09-11 17:45:25 +0300

    добавлена модель, сервис и контроллер для лицензий + интернационализация

--- src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -2,6 +2,12 @@ package com.optimagrowth.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class LicenseServiceApplication {
@@ -10,4 +16,29 @@ public class LicenseServiceApplication {
 		SpringApplication.run(LicenseServiceApplication.class, args);
 	}
 
+
+	/*Интернационализация – важное требование, позволяющее приложению адаптироваться к различным языкам.
+	 Основная цель интернационализации – организовать в приложении возможность возвращать контент в
+	 разных форматах и на разных языках.*/
+
+	//Устанавливает локаль US как локаль по умолчанию
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource =
+				new ResourceBundleMessageSource();
+
+		//Не генерирует ошибку, если сообщение не найдено, а возвращает код сообщения вместо этого
+		messageSource.setUseCodeAsDefaultMessage(true);
+
+		//Задает базовое имя файлов с переводами сообщений на разные языки
+		messageSource.setBasenames("messages");
+		return messageSource;
+	}
 }
